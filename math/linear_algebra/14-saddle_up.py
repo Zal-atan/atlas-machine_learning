b53f762c1c35f149a631239d216b39a281bd53ec
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2024-01-02 23:00:15 +0000

    Adding file 14-saddle_up.py

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module creates function np_matmul, which does matrix multiplication
+using numpy"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """This functions multiplies two input matrices using numpy and returns
+    the resultant matrix"""
+    return np.matmul(mat1, mat2)
