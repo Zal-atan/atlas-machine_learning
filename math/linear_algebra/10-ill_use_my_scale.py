95b68e918662f9aee2c75bcd2430267efb8ca90a
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2024-01-02 22:17:55 +0000

    Adding file 10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module creates a function to return the shape of a matrix"""
+
+
+def np_shape(matrix):
+    """This function returns the shape of an input matrix as a tuple"""
+    return matrix.shape
