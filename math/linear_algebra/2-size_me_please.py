02461d46a6c8cf39357998d5c2482bca1e856ee4
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2024-01-02 17:53:46 +0000

    Adding file 2-size_me_please.py

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""This module makes a function which can find the size of matrix, given that
+all the elements of the same dimension are of the same type/shape"""
+
+def matrix_shape(matrix):
+    """Takes in a matrix of equal type/shape in each dimension and returns
+    the shape of the matrix"""
+    shape = []
+    shape.append(len(matrix))
+    new_matrix = matrix
+    while True:
+        try:
+            new_matrix = new_matrix[0]
+            shape.append(len(new_matrix))
+        except:
+            break
+    return shape
