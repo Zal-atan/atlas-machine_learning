4d8879ad4d5e72a5448e05da0937f6689439eb4e
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2024-01-02 19:18:07 +0000

    Adding file 7-getting_cozy.py

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This module creates the function cat_arrays(arr1, arr2) which adds
+two arrays together"""
+
+def cat_arrays(arr1, arr2):
+    """This functions takes two arrays (arr1, arr2) and adds them together,
+    and then returns the result"""
+    return arr1 + arr2
---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""This module creates the cat_matrices2D function, which combines two
+2-dimensional matrices based on the given axis."""
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """Takes two matrices as inputs (mat1, mat2) and combines them on the
+    optional axis given. Axis is set to 0 by default. Axis = 0 adds the matrix
+    as an extra row onto the old matrix, where Axis = 1 adds the the matrix as
+    an extra column to each matrix. If the two matrices cannot be concatenated,
+    returns None, else returns the concatenated matrix"""
+
+    try:
+        new_matrix = []
+        if axis == 0:
+            for matrix in mat1:
+                new_matrix.append(matrix.copy())
+            for matrix in mat2:
+                new_matrix.append(matrix.copy())
+        elif axis == 1:
+            if len(mat2) > len(mat1):
+                print("Went here {}".format(mat2))
+                return None
+            for i in range(len(mat1)):
+                new_array = mat1[i].copy()
+                new_array.append(mat2[i][0])
+                new_matrix.append(new_array)
+                # print(new_array, new_matrix)
+        return new_matrix
+
+    except:
+        return None
