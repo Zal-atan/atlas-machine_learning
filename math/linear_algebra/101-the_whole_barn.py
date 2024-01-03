df3feade9860165b34849d8ba42c563a817adda9
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2024-01-03 23:27:20 +0000

    Adding matrix addition

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,10 +1,52 @@
 #!/usr/bin/env python3
-"""This module uses numpy to added matrices"""
-import numpy as np
+"""This module adds two matrices"""
+import pdb
+
+
+def matrix_shape(matrix):
+    """Takes in a matrix of equal type/shape in each dimension and returns
+    the shape of the matrix"""
+    shape = []
+    shape.append(len(matrix))
+    new_matrix = matrix
+    while True:
+        try:
+            new_matrix = new_matrix[0]
+            shape.append(len(new_matrix))
+        except Exception:
+            break
+    return shape
 
 
 def add_matrices(mat1, mat2):
-    try:
-        return np.add(mat1, mat2)
-    except Exception:
+    """Function for adding mat1 to mat2, returns resultant matrix"""
+    shape1 = matrix_shape(mat1)
+    shape2 = matrix_shape(mat2)
+    if shape1 != shape2:
         return None
+
+    new_matrix = []
+    for i in range(shape1[0]):
+        first_tier = []
+        try:
+            for j in range(shape1[1]):
+                second_tier = []
+                try:
+                    for k in range(shape1[2]):
+                        third_tier = []
+                        try:
+                            for l in range(shape1[3]):
+                                third_tier.append(mat1[i][j][k][l] + mat2[i][j][k][l])
+                        except:
+                            second_tier.append(mat1[i][j][k] + mat2[i][j][k])
+                        else:
+                            second_tier.append(third_tier)
+                except:
+                    first_tier.append(mat1[i][j] + mat2[i][j])
+                else:
+                    first_tier.append(second_tier)
+        except:
+            new_matrix.append(mat1[i] + mat2[i])
+        else:
+            new_matrix.append(first_tier)
+    return new_matrix
