18117ade48a6f212fb19744538edbe7766de1486
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2024-01-03 17:58:56 +0000

    Adding file 4-frequency.py

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+# your code here
+plt.hist(student_grades,
+         bins=5,
+         color="dodgerblue",
+         edgecolor="black",
+         range=(40,100))
+plt.xlim(0, 100)
+plt.ylim(0, 30)
+plt.title("Project A")
+plt.xlabel("Grades")
+plt.ylabel("Number of Students")
+plt.show()
