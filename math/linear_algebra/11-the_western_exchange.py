a7d7fbe665de9ca9d63a3d8bc3d60a4dc3324ecd
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2024-01-03 17:34:13 +0000

    Fixed 11 to not import np

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """Module creating function which returns a transposed matrix"""
-import numpy as np
 
 
 def np_transpose(matrix):
     """Returns a transposed matrix from input matrix"""
-    return np.transpose(matrix)
+    return matrix.transpose()
