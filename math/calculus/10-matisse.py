e5898d864aba2a49e1dc48575ec9c24c7093db3a
Zal-Atan <ethanzalta@gmail.com>
2024-01-08 12:04:33 -0600

    Should have gotten edge cases for 10

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -2,15 +2,23 @@
 """Create function poly_derivative(poly), which will return the derivative
 of a list of polynomials."""
 
+
 def poly_derivative(poly):
     """ Returns a list of derivatives of polynomials, input is a list.
     Example: x^3 + 3x +5 would be input as [5, 3, 0, 1]
     Output would be: [3, 0, 3]"""
 
-    return_list = []
+    if not isinstance(poly, list):
+        return None
+
     for value in poly:
         if not isinstance(value, int):
             return None
+
+    if len(poly) == 1:
+        return [0]
+
+    return_list = []
     for i in range(1, len(poly)):
         return_list.append(i * poly[i])
 
