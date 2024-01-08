853a8bffcc24354bcc03f7fdf96eec187f1c18f5
Zal-Atan <ethanzalta@gmail.com>
2024-01-08 11:50:19 -0600

    Trying a fix for one part of 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-""" In this module I will create a summation of i^2 function"""
+""" Creating a functions which calculates sum of the total of the square of
+1 to n"""
 
-
+@__cached__
 def summation_i_squared(n):
     """Takes the squares of all values of from 1 to n and returns a sum of
        each"""
-    if not isinstance(n, int):
+    if not isinstance(n, int) or (n < 1):
         return None
     if n == 1:
         return n
