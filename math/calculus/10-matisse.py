9a35c185b6179e0322f217865e1c202e3a60d671
Zal-Atan <ethanzalta@gmail.com>
2024-01-08 12:21:18 -0600

    Reworking 10 backwards to hopefully get all checks

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -8,18 +8,28 @@ def poly_derivative(poly):
     Example: x^3 + 3x +5 would be input as [5, 3, 0, 1]
     Output would be: [3, 0, 3]"""
 
-    if not isinstance(poly, list):
+    if isinstance(poly, list):
+        if len(poly) == 1:
+            return [0]
+        return_list = []
+        for i in range(1, len(poly)):
+            return_list.append(i * poly[i])
+
+    else:
         return None
 
-    for value in poly:
-        if not isinstance(value, int):
-            return None
+    # if not isinstance(poly, list):
+    #     return None
 
-    if len(poly) == 1:
-        return [0]
+    # for value in poly:
+    #     if not isinstance(value, int):
+    #         return None
 
-    return_list = []
-    for i in range(1, len(poly)):
-        return_list.append(i * poly[i])
+    # if len(poly) == 1:
+    #     return [0]
 
-    return return_list
+    # return_list = []
+    # for i in range(1, len(poly)):
+    #     return_list.append(i * poly[i])
+
+    # return return_list
