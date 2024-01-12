53667448ee618a02b2ceb1c6496959a078cea469
Zal-Atan <ethanzalta@gmail.com>
2024-01-12 11:26:58 -0600

    Fixed pycodestyle for task 0

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Module for creating class Poisson"""
 
+
 class Poisson():
     """Creates Poisson class"""
 
     def __init__(self, data=None, lambtha=1.):
         """Initializes Poisson class with data (list) values and a lambtha"""
-        #initialize lambtha
+        # initialize lambtha
         if not lambtha > 0:
             raise ValueError("lambtha must be a positive value")
         self.lambtha = lambtha
@@ -16,5 +17,5 @@ class Poisson():
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
-                raise ValueError("data must be a list")
+                raise ValueError("data must contain multiple values")
             self.lambtha = sum(data) / len(data)
