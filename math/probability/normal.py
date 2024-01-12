1e481839aa267589908cefa2c9615f9aed031f55
Zal-Atan <ethanzalta@gmail.com>
2024-01-12 13:01:05 -0600

    Adding normal.py for task 6

---
+++ math/probability/normal.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Module creating a normal class"""
+
+
+class Normal():
+    """Creating a class representing a normal distribution"""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """Initializes Normal class with a value of mean and stddev either
+        given or produced from the input list (data)"""
+        self.mean = mean
+        self.stddev = stddev
+
+        # check if data
+        if data is not None:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            n = len(data)
+            self.mean = sum(data) / n
+            self.stddev = (1/n * sum(map(lambda x:
+                                         ((data[x] - self.mean) ** 2),
+                                         range(0, n)))) ** .5
