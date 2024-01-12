2a76ec761aee5c7a915ca4849afc519106cf8833
Zal-Atan <ethanzalta@gmail.com>
2024-01-12 11:24:35 -0600

    Adding task 0 - Poisson

---
+++ math/probability/poisson.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module for creating class Poisson"""
+
+class Poisson():
+    """Creates Poisson class"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """Initializes Poisson class with data (list) values and a lambtha"""
+        #initialize lambtha
+        if not lambtha > 0:
+            raise ValueError("lambtha must be a positive value")
+        self.lambtha = lambtha
+
+        # check if data
+        if data is not None:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must be a list")
+            self.lambtha = sum(data) / len(data)
