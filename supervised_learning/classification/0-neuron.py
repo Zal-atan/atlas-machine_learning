9303705ec2d4ba9e3614f2de51f4f66b864fd0c9
Zal-Atan <ethanzalta@gmail.com>
2024-01-21 16:07:17 -0600

    Changing location of data directory and finished file 0-neuron.py

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+""" Module creating a class Neuron"""
+import numpy as np
+
+
+class Neuron():
+    """ Defines a single neuron"""
+
+    def __init__(self, nx):
+        """
+        Initiates class Neuron
+        nx is the number of input features to the neuron. Must be an integer
+        of greater than or equal to 1
+        """
+        nx_is_int = isinstance(nx, int)
+        nx_ge_1 = nx >= 1
+        if not nx_is_int:
+            raise TypeError("nx must be an integer")
+        if not nx_ge_1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
