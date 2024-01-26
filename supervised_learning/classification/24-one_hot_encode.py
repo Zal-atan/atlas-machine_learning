c51d71f3a2580624bdc60e43398960583e278678
Zal-Atan <ethanzalta@gmail.com>
2024-01-26 12:58:45 -0600

    Adding file 25 - one hot

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Creates a function one_hot_encode"""
+import numpy as np
+
+def one_hot_encode(Y, classes):
+    """
+    Converts a numeric label vector into a one-hot matrix
+
+    Inputs:
+    Y - numpy.ndarray containing numeric class labels
+    classes - the maximum number of classes found in Y
+
+    Returns:
+    one-hot encoding of Y with shape (classes, m), or None
+    """
+    if not isinstance(classes, int) or not isinstance(Y, np.ndarray):
+        return None
+
+    try:
+        one_hot = np.eye(classes)[Y].T
+        return one_hot
+    except Exception as e:
+        return None
