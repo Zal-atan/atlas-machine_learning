e3659c535a68041f41ed880b878ba30b39e85047
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2024-02-06 14:21:14 -0600

    Adding 4 and 5

--- supervised_learning/tensorflow/5-create_train_op.py
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 """
-Create a function calculate_loss(y, y_pred):
+Create a function create_train_op(loss, alpha):
 """
 import tensorflow.compat.v1 as tf
+
+
+def create_train_op(loss, alpha):
+    """
+    Creates the training operation for the network:
+
+    Inputs:
+    loss - the loss of the networks prediction
+    alpha - the learning rater
+
+    Returns:
+    operation that trains the network using gradient descent
+    """
+
+    optimizer = tf.train.GradientDescentOptimizer(alpha)
+    return optimizer.minimize(loss)
