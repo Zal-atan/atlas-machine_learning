f8703d64458b5eaaf2338c3e76ed720314794d44
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2024-02-07 11:36:55 -0600

    Trying 7 again

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -16,18 +16,18 @@ def evaluate(X, Y, save_path):
 
     Returns - the network's prediction, accuracy, and loss respectively
     """
-    sess = tf.Session()
-    saver = tf.train.import_meta_graph(save_path + '.meta')
-    saver.restore(sess, save_path)
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(save_path + '.meta')
+        saver.restore(sess, save_path)
 
-    x = tf.get_collection('x')[0]
-    y = tf.get_collection('y')[0]
-    y_pred = tf.get_collection('y_pred')[0]
-    accuracy = tf.get_collection('accuracy')[0]
-    loss = tf.get_collection('loss')[0]
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        loss = tf.get_collection('loss')[0]
 
-    evaluate_predict = sess.run(y_pred, feed_dict={x: X, y: Y})
-    evaluate_accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
-    evaluate_loss = sess.run(loss, feed_dict={x: X, y: Y})
+        evaluate_predict = sess.run(y_pred, feed_dict={x: X, y: Y})
+        evaluate_accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
+        evaluate_loss = sess.run(loss, feed_dict={x: X, y: Y})
 
     return evaluate_predict, evaluate_accuracy, evaluate_loss
