0813b4cdc2bbb54977e1ba7f8da9e52d356dd8d0
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2024-02-11 16:47:06 -0600

    Adding file 6

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" This module creates update_variables_RMSProp(alpha, beta2, epsilon,
+var, grad, s): function"""
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """
+    updates a variable using the RMSProp optimization algorithm
+
+    Inputs:
+    alpha - learning rate
+    beta2 - RMSProp weight
+    epsilon - small number to avoid division by zero
+    var - numpy.ndarray containing the variable to be updated
+    grad - numpy.ndarray containing the gradient of var
+    s - previous second moment of var
+
+    Returns:
+    The updated variable and the new moment, respectively
+    """
+    
---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" This module creates create_momentum_op(loss, alpha, beta1) function"""
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """
+    Creates the training operation for a neural network in tensorflow using
+    the gradient descent with momentum optimization algorithm.
+
+    Inputs:
+    loss - loss of the network
+    alpha - learning rate
+    beta1 - momentum weight
+
+    Returns:
+    The momentum optimization operation
+    """
+    optimized = tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
+    return optimized
