35bc498a1d60e71846c853fd3cee511528f5f5ee
Zal-Atan <ethanzalta@gmail.com>
2024-02-12 15:59:32 -0600

    Finally figured out 9, incorrect returns

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -2,6 +2,7 @@
 """ This module creates update_variables_Adam(alpha, beta1, beta2, epsilon,
 var, grad, v, s, t) function
 """
+import numpy as np
 
 
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
@@ -24,13 +25,12 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     The updated variable, the new first moment, and the new second moment,
     respectively
     """
-    V = (beta1 * v) + ((1 - beta1) * grad)
-    S = (beta2 * s) + ((1 - beta2) * (grad ** 2))
+    V = beta1 * v + (1 - beta1) * grad
+    S = beta2 * s + (1 - beta2) * (grad ** 2)
 
     V_corrected = V / (1 - (beta1 ** t))
     S_corrected = S / (1 - (beta2 ** t))
 
-    updated_var = var - alpha * (V_corrected / ((S_corrected ** (1 / 2))
-                                                + epsilon))
+    var -= alpha * (V_corrected / (np.sqrt(S_corrected) + epsilon))
 
-    return updated_var, V_corrected, S_corrected
+    return var, V, S
