c36b79ac70ea471ab38d9580471483bc9c78fc09
Zal-Atan <ethanzalta@gmail.com>
2024-02-20 13:19:26 -0600

    Another try on 3

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -2,6 +2,7 @@
 """This module will create the l2_reg_create_layer function"""
 import tensorflow.compat.v1 as tf
 
+
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
     Creates a tensorflow layer that includes L2 regularization
@@ -17,9 +18,10 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     """
 
     regularizer = tf.keras.regularizers.l2(lambtha)
-    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode="fan_avg")
+    init = tf.variance_scaling_initializer(scale=2.0, mode="fan_avg")
 
-    layer = tf.keras.layers.Dense(units=n, activation=activation,
-                                  kernel_initializer=init,
-                                  kernel_regularizer=regularizer)
+    layer = tf.layers.Dense(units=n, activation=activation,
+                            kernel_initializer=init,
+                            kernel_regularizer=regularizer)
+
     return layer(prev)
