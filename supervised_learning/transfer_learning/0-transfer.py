19be46069295a1a5d7255978a98bad6db00c6106
Zal-atan <ethanzalta@gmail.com>
2024-03-25 11:35:15 -0500

    Getting up to 81% accuracy

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -4,6 +4,10 @@ CIFAR 10 dataset"""
 import tensorflow.keras as K
 
 
+epochs = 15
+batch_size = 64
+img_reshape = 160
+
 def preprocess_data(X, Y):
     """
     Pre-processes the data for your model
@@ -29,17 +33,23 @@ if __name__ == "__main__":
 
     inputs = K.Input(shape=(32, 32, 3))
     input_resized = K.layers.Lambda(lambda image: K.backend.resize_images(
-        image, 160/32, 160/32, "channels_last"))(inputs)
+        image, img_reshape/32, img_reshape/32, "channels_last"))(inputs)
     
     base_model = K.applications.DenseNet121(include_top=False,
                                              weights='imagenet',
                                              input_tensor=input_resized,
-                                             input_shape=(160, 160, 3),
+                                             input_shape=(img_reshape,
+                                                          img_reshape, 3),
                                              pooling='max')
     base_model.trainable = False
 
     model = K.models.Sequential()
     model.add(base_model)
+    model.add(K.layers.BatchNormalization())
+    # model.add(K.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(K.layers.Dropout(0.25))
+
+
     model.add(K.layers.Flatten())
     model.add(K.layers.Dense(512, activation=('relu')))
     # model.add(K.layers.Dropout(0.5))
@@ -48,18 +58,13 @@ if __name__ == "__main__":
     model.add(K.layers.Dense(10, activation=('softmax')))
     callback = []
 
-
-    # def rate_decay(epoch):
-    #     """ Decay for learning rate """
-    #     return 0.001 / (1 + 1 * 30)
     learn_rate_reduce = K.callbacks.ReduceLROnPlateau(monitor='val_accuracy',
-                                        factor=.01,
+                                        factor=.11,
                                         patience=2,
                                         min_lr=1e-5)
+    callback.append(learn_rate_reduce)
 
 
-    # learning = K.callbacks.LearningRateScheduler(schedule=rate_decay, verbose=1)
-    # callback.append(learning)
     callback.append(K.callbacks.ModelCheckpoint('cifar10.h5',
                                                 monitor='val_accuracy',
                                                 save_best_only=True,
@@ -69,8 +74,8 @@ if __name__ == "__main__":
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
-    history = model.fit(X_train, Y_train, epochs=15, batch_size=128,
-                        validation_data=(X_test, Y_test))
+    history = model.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size,
+                        validation_data=(X_test, Y_test), callbacks=callback)
 
     model.save('cifar10.h5')
 
