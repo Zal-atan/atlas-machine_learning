87eedd38d30cecebe9f2acec0b23be3e4191eca3
Zal-atan <ethanzalta@gmail.com>
2024-04-04 14:35:07 -0300

    Fixing pycodestyle

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -8,21 +8,21 @@ class Yolo:
     """ Uses the Yolo v3 algorithm to perform object detection"""
 
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        """ 
+        """
         Initializer Yolo Class
 
         Inputs:
         model_path: path to where a Darknet Keras model is stored
-        classes_path: path to where the list of class names used for the 
+        classes_path: path to where the list of class names used for the
             Darknet model, listed in order of index, can be found
-        class_t: float representing the box score threshold for the initial 
+        class_t: float representing the box score threshold for the initial
             filtering step
         nms_t: float representing the IOU threshold for non-max suppression
-        anchors: numpy.ndarray of shape (outputs, anchor_boxes, 2) 
+        anchors: numpy.ndarray of shape (outputs, anchor_boxes, 2)
             containing all of the anchor boxes:
             outputs: number of outputs (predictions) made by the Darknet model
             anchor_boxes: number of anchor boxes used for each prediction
-            2: [anchor_box_width, anchor_box_height] 
+            2: [anchor_box_width, anchor_box_height]
         """
         # Load model
         self.model = K.models.load_model(model_path)
@@ -35,4 +35,3 @@ class Yolo:
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
-
