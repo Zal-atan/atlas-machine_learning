1975d2210d47278b4bcb4a08e4c914cbe69bb4ce
Zal-atan <ethanzalta@gmail.com>
2024-05-02 13:05:40 -0500

    Finishing 0-determinate.py

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -16,8 +16,28 @@ def multi_determinate(matrix):
 
     if len(matrix) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
-    
-    return 1
+
+    multiplier = 1
+    determ = 0
+    for i in range(len(matrix)):
+        leader = matrix[0][i]
+        sub_matrix = []
+
+        for row in range(len(matrix)):
+            if row == 0:  # Skip the leader row
+                continue
+            new_row = []
+
+            for col in range(len(matrix)):
+                if col == i:  # Skip the leader column
+                    continue
+                new_row.append(matrix[row][col])
+
+            sub_matrix.append(new_row)
+        determ += (multiplier * leader * multi_determinate(sub_matrix))
+        multiplier *= -1
+
+    return determ
 
 
 def determinant(matrix):
@@ -25,23 +45,23 @@ def determinant(matrix):
     Calculates the determinate of a matrix.
 
     Input:
-    matrix - list of lists whose determinant should be calculated, must be 
+    matrix - list of lists whose determinant should be calculated, must be
         square
 
     Returns:
     The determinant of the matrix
     """
-    
-    if type(matrix) != list or len(matrix) == 0:
+
+    if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
-    
+
     if matrix == [[]]:
         return 1
-    if len(matrix) == 1 and len(matrix[0]) ==1:
+    if len(matrix) == 1 and len(matrix[0]) == 1:
         return matrix[0][0]
-    
+
     for mat in matrix:
-        if type(mat) != list:
+        if not isinstance(mat, list):
             raise TypeError("matrix must be a list of lists")
         if len(mat) != len(matrix[0]) or len(mat) != len(matrix):
             raise ValueError("matrix must be a square matrix")
