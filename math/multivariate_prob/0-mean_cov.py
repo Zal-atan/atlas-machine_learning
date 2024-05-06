2de06bdd40c5b75ebd3379493c4fe900d6ce37c6
Zal-atan <ethanzalta@gmail.com>
2024-05-06 14:14:07 -0500

    Adding file 0-mean_cov.py

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -17,3 +17,14 @@ def mean_cov(X):
     cov - numpy.ndarray of shape (d, d) containing the covariance matrix
         of the data set
     """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+
+    n, d = X.shape
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+
+    mean = np.mean(X, axis=0, keepdims=True)
+    cov = (1 / (n - 1)) * np.matmul(X.T - mean.T, X - mean)
+
+    return mean, cov
