ac6395f69b2d349376d577a55d6a59573f6c5074
Zal-atan <ethanzalta@gmail.com>
2024-05-14 15:13:17 -0500

    removing import of file 0 in file 1 and adding the function

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python3
 """ This module creates the kmeans function"""
 import numpy as np
-initialize = __import__('0-initialize').initialize
+
+
+def initialize(X, k):
+    """
+    Initializes cluster centroids for K-means
+
+    Inputs:
+    X - numpy.ndarray of shape (n, d) containing the dataset that will be
+    used for K-means clustering
+        n - number of data points
+        d - number of dimensions for each data point
+    k - positive integer containing the number of clusters
+
+    Returns:
+    numpy.ndarray of shape (k, d) containing the initialized centroids for
+    each cluster, or None on failure
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(k, int) or k <= 0:
+        return None
+
+    min_val = X.min(axis=0)
+    max_val = X.max(axis=0)
+
+    centroid = np.random.uniform(min_val, max_val, size=(k, X.shape[1]))
+
+    return centroid
 
 
 def kmeans(X, k, iterations=1000):
