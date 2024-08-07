54ea6694cf6bd8f45befa5d571a08d879d649c82
Zal-atan <ethanzalta@gmail.com>
2024-08-07 11:00:20 -0500

    Adding file 0-data.py

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -1,1 +1,45 @@
 #!/usr/bin/env python3
+""" Module creating a Dataset Class """
+
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+
+
+class Dataset():
+    """Loads and preps a dataset for machine translation"""
+
+    def __init__(self):
+        """ Initialize Dataset Class"""
+
+        examples, _ = tfds.load('ted_hrlr_translate/pt_to_en',
+                                with_info=True,
+                                as_supervised=True)
+
+        self.data_train, self.data_valid = examples['train'], \
+            examples['validation']
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train)
+
+    def tokenize_dataset(self, data):
+        """
+        Creates sub-word tokenizers for the dataset. Maximum Vocab size should
+        be set to 2**15
+
+        Input:
+        data: tf.data.Dataset whose examples are formatted as a tuple
+        (pt, en)\\
+            pt: tf.Tensor containing the Portuguese sentence\\
+            en: tf.Tensor containing the corresponding English sentence
+
+        Returns:
+            tokenizer_pt: The Portuguese tokenizer
+            tokenizer_en: The English tokenizer
+        """
+        tokenizer_pt = tfds.deprecated.text.\
+            SubwordTextEncoder.build_from_corpus(
+                (pt.numpy() for pt, _ in data), target_vocab_size=2**15)
+        tokenizer_en = tfds.deprecated.text.\
+            SubwordTextEncoder.build_from_corpus(
+                (en.numpy() for _, en in data), target_vocab_size=2**15)
+
+        return tokenizer_pt, tokenizer_en
