30f7a3b8d41d8162d47a9a11dd4b717416ec5374
Zal-atan <ethanzalta@gmail.com>
2024-08-13 14:37:30 -0500

    Added 2-qa.py

--- supervised_learning/qa_bot/2-qa.py
+++ supervised_learning/qa_bot/2-qa.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+""" First basic version of the question/answer bot """
+
+question_answer = __import__('0-qa').question_answer
+
+
+def answer_loop(reference):
+    """
+    Answers questions from a reference text
+
+    Inputs:\\
+    reference is the reference text
+    """
+    while True:
+        question = input("Q: ")
+        exits = ["exit", "quit", 'goodbye', 'bye']
+        if question.lower() in exits:
+            print("A: Goodbye")
+            break
+        answer = question_answer(question, reference)
+        if answer == None or answer == "":
+            answer = "Sorry, I do not understand your question."
+        print(f"A: {answer}")
