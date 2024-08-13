213e92f699c89598652f39a0b2a1f63fb022eb01
Zal-atan <ethanzalta@gmail.com>
2024-08-13 12:16:30 -0500

    Addomg 1-loop.py

--- supervised_learning/qa_bot/1-loop.py
+++ supervised_learning/qa_bot/1-loop.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+""" Module with basic QA bot exit feature """
+
+
+while True:
+    question = input("Q: ")
+    exits = ["exit", "quit", 'goodbye', 'bye']
+    if question.lower() in exits:
+        print("A: Goodbye")
+        break
+    print("A: ")
