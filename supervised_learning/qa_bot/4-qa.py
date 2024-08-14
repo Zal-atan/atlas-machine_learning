63ad7217f2f9c956ff83668cd7fed43a5714771b
Zal-atan <ethanzalta@gmail.com>
2024-08-14 13:55:33 -0500

    Updating 4-qa.py, does not fully work because semantic_search not fully working

--- supervised_learning/qa_bot/4-qa.py
+++ supervised_learning/qa_bot/4-qa.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+""" Module creating the Q?A bot final verison """
+
+quest_ans = __import__('0-qa').question_answer
+semantic_search = __import__('3-semantic_search').semantic_search
+
+def question_answer(corpus_path):
+    """
+    Answers questions from multiple reference texts
+
+    Inputs:\\
+    corpus_path: the path to the corpus of reference documents
+    """
+    while True:
+        question = input("Q: ")
+        exits = ["exit", "quit", 'goodbye', 'bye']
+        if question.lower() in exits:
+            print("A: Goodbye")
+            break
+        reference = semantic_search(corpus_path, question)
+        answer = quest_ans(question, reference)
+        if answer == None or answer == "":
+            answer = "Sorry, I do not understand your question."
+        print(f"A: {answer}")
