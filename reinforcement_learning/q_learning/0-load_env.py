5d9848c6626087b541313a5bacd275fbbd65cf1c
Zal-atan <ethanzalta@gmail.com>
2024-08-27 12:13:55 -0500

    Adding new repo Q-Learning, README, files 0 and 1

---
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Module for creating the load_frozen_lake function """
+
+import gym
+
+
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    """
+    Loads the pre-made FrozenLakeEnv evnironment from OpenAIs gym
+
+    Inputs:\\
+    desc: either None or a list of lists containing a custom description of 
+        the map to load for the environment\\
+    map_name: either None or a string containing the pre-made map to load\\
+    is_slippery: boolean to determine if the ice is slippery
+
+    Returns:
+    the environment
+    """
+    env = gym.make('FrozenLake-v1', desc=desc, map_name=map_name,
+                   is_slippery=is_slippery)
+    
+    return env
---
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Module for creating the q_init function """
+
+import numpy as np
+
+
+def q_init(env):
+    """
+    Initializes the Q-table
+
+    Inputs:\\
+    env: the FrozenLakeEnv instance
+
+    Returns:
+    the Q-table as a numpy.ndarray of zeros
+    """
+    
+    action = env.action_space.n
+    obv_space = env.observation_space.n
+
+    return np.zeros((obv_space, action))
