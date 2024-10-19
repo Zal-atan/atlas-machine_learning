480eb820000140b9d8ad669eea0176b3cecdc81d
Zal-atan <ethanzalta@gmail.com>
2024-10-19 16:01:43 -0500

    Making all SQL Uppercase on 10 and 11

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT TV_GENRES.name as genre, count(TV_SHOW_GENRES.show_id) as number_of_shows
+SELECT TV_GENRES.name AS genre, count(TV_SHOW_GENRES.show_id) AS number_of_shows
 FROM TV_GENRES
 LEFT JOIN TV_SHOW_GENRES ON TV_GENRES.id = TV_SHOW_GENRES.genre_id
 GROUP BY TV_GENRES.name
--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -1,7 +1,7 @@
 -- Lists all shows from hbtn_0d_tvshows_rate by their rating
-SELECT tv_shows.title, sum(tv_show_ratings.rate) as rating
+SELECT tv_shows.title, sum(tv_show_ratings.rate) AS rating
 FROM tv_shows
-LEFT JOIN tv_show_ratings on tv_shows.id = tv_show_ratings.show_id
+LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
 ORDER BY rating DESC;
 
